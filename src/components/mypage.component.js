d68471bc981de5d293728b89a8d763730ac161d5
riki <riki@rikinoMacBook-Air.local>
2020-05-17 09:28:13 +0900

    modified table size

--- src/components/mypage.component.js
+++ src/components/mypage.component.js
@@ -40,37 +40,35 @@ class Mypage extends Component {
             		<DropDown key="name" index={this.state.name} />
 				</div>
 	        	<h1 className="text-center">COVID19　Summary</h1>
-				<div className="table-responsive wrap-table">
-				　　<table className="table table-condensed table-striped">
-						<thead>
-							<tr>
-							    <th colspan="2" className="text-center">Country</th>
-							    <th>NewConfirmed</th>
-							    <th>TotalConfirmed</th>
-							    <th>NewDeaths</th>
-							    <th>TotalDeaths</th>
-							    <th>NewRecovered</th>
-							    <th>TotalRecovered</th>
-							</tr>
-						</thead>
-						<tbody className="text-right">
-							<tr className="active text-danger">
-							    <td colspan="2">{this.state.country + this.state.date}</td>
-							    <td>{this.state.newConfirmed}</td>
-							    <td>{this.state.totalConfirmed}</td>
-							    <td>{this.state.newDeaths}</td>
-							    <td>{this.state.totalDeaths}</td>
-							    <td>{this.state.newRecovered}</td>
-							    <td>{this.state.totalRecovered}</td>
-							</tr>
+			　　<table className="table table-condensed table-striped table-responsive wrap-table">
+					<thead>
+						<tr>
+						    <th colSpan="2" className="text-center">Country</th>
+						    <th>NewConfirmed</th>
+						    <th>TotalConfirmed</th>
+						    <th>NewDeaths</th>
+						    <th>TotalDeaths</th>
+						    <th>NewRecovered</th>
+						    <th>TotalRecovered</th>
+						</tr>
+					</thead>
+					<tbody className="text-right">
+						<tr className="active text-danger">
+						    <td colSpan="2">{this.state.country + this.state.date}</td>
+						    <td>{this.state.newConfirmed}</td>
+						    <td>{this.state.totalConfirmed}</td>
+						    <td>{this.state.newDeaths}</td>
+						    <td>{this.state.totalDeaths}</td>
+						    <td>{this.state.newRecovered}</td>
+						    <td>{this.state.totalRecovered}</td>
+						</tr>
 
-							{this.state.countries.map((country, index) => {
-      							return <SummaryList key={index} index={country} />;
-   	 						})}							
+						{this.state.countries.map((country, index) => {
+  							return <SummaryList key={index} index={country} />;
+	 						})}							
 
-						</tbody>
-				　　</table>
-				</div>
+					</tbody>
+			　　</table>
             </div>
         );
 
